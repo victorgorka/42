01bd3f732e5c747610e5ac1c88168182fb668f66
Victor gorka De prada arnedo <vde-prad@student.42malaga.com>
2022-05-12 18:35:04 +0200

    empezando split

---
+++ ft_split.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-prad <vde-prad@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/12 16:45:28 by vde-prad          #+#    #+#             */
+/*   Updated: 2022/05/12 17:02:20 by vde-prad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+char	**ft_split(char const *s, char c)
+{
+	char **tab;
+
+	tab = (char **)malloc(8 * sizeof(char *));
+
+}
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	printf("size of char *: %lu\n", sizeof(char *));
+}
