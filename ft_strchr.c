285812d6669fb23c765a4d581c4d390b2d73896f
Victor gorka De prada arnedo <vde-prad@student.42malaga.com>
2022-05-12 16:34:15 +0200

    arreglo ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,16 +6,12 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 14:59:17 by vde-prad          #+#    #+#             */
-/*   Updated: 2022/05/09 15:44:09 by vde-prad         ###   ########.fr       */
+/*   Updated: 2022/05/12 13:59:00 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strchr(const char *s, int c)
 {
-	if (*s == 0 && c != 0)
-		return (0);
-	else if (*s == 0 && c == 0)
-		return ("");
 	while (*s != 0)
 	{
 		if (*s == (unsigned char)c)
@@ -24,6 +20,8 @@ char	*ft_strchr(const char *s, int c)
 		if (*s == (unsigned char)c)
 			return ((char *)s);
 	}
+	if (c == 0)
+		return ((char *)s);
 	return (0);
 }
 /*
