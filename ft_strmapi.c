941c6370d3e2ead7ca4b185f0f3949316effefac
Victor gorka De prada arnedo <vde-prad@student.42malaga.com>
2022-05-25 18:46:52 +0200

    strmapi avanzando

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,11 +6,33 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 13:47:34 by vde-prad          #+#    #+#             */
-/*   Updated: 2022/05/25 13:48:01 by vde-prad         ###   ########.fr       */
+/*   Updated: 2022/05/25 14:52:34 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
+char	fun(unsigned int i, char c)
+{
+	return (c + 1);
+}
+
+/*
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 
+}
+*/
+
+void	print(char *str, char (*f)(unsigned int, char))
+{
+	while (*str)
+		printf("%c", fun(0, *str++));
+}
+
+
+int main()
+{
+	char *str = "hola perita";
+	print(str, fun);
 }
