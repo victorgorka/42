066d04f494be2b01a40b6212fdefeb36d0d0a933
Victor gorka De prada arnedo <vde-prad@student.42malaga.com>
2022-06-02 18:43:57 +0200

    lstadd_front finished

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -6,7 +6,7 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 14:49:35 by vde-prad          #+#    #+#             */
-/*   Updated: 2022/05/31 13:20:19 by vde-prad         ###   ########.fr       */
+/*   Updated: 2022/06/01 18:48:13 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -16,10 +16,11 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 	new->next = *lst;
 	*lst = new;
 }
-/*
+
 #include<stdio.h>
 int main()
 {
+	/*
 	t_list	*a, *b, *c, *z;
 
 	z = ft_lstnew("0");
@@ -28,7 +29,16 @@ int main()
 	a = ft_lstnew("5");
 	b->next = c;
 	a->next = b;
-	ft_lstadd_front(&z, a);
-	puts(a->content);
+	ft_lstadd_front(&a, z);
+	puts(z->next->content);
+	*/
+
+	t_list *list;
+
+	ft_lstadd_front(&list, ft_lstnew("15"));
+	ft_lstadd_front(&list, ft_lstnew("10"));
+	ft_lstadd_front(&list, ft_lstnew("5"));
+	ft_lstadd_front(&list, ft_lstnew("0"));
+	puts(list->content);
 }
-*/
+
