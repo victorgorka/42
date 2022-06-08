43ba30054d7065413d0844abeb39e76896561228
Victor gorka De prada arnedo <vde-prad@student.42malaga.com>
2022-06-08 13:11:26 +0200

    lstclear finished

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -6,22 +6,19 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 17:35:11 by vde-prad          #+#    #+#             */
-/*   Updated: 2022/06/07 17:51:37 by vde-prad         ###   ########.fr       */
+/*   Updated: 2022/06/08 13:10:42 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	if ((*lst)->next == 0)
-		ft_lstdelone(*lst, del);
-	else
+	t_list	*cpy;
+
+	while (*lst != 0)
 	{
-		while ((*lst)->next != 0)
-		{
-			ft_lstdelone(*lst, del);
-			*lst = (*lst)->next;
-		}
+		cpy = (*lst)->next;
 		ft_lstdelone(*lst, del);
+		*lst = cpy;
 	}
 }
--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,12 +6,12 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 16:52:54 by vde-prad          #+#    #+#             */
-/*   Updated: 2022/06/07 17:18:17 by vde-prad         ###   ########.fr       */
+/*   Updated: 2022/06/08 13:11:03 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-void ft_lstdelone(t_list *lst, void (*del)(void *))
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	del(lst->content);
 	free(lst);
