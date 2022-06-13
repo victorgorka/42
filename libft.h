07bff8f72d987144a69625d8062d001e68961adf
Victor gorka De prada arnedo <vde-prad@student.42malaga.com>
2022-06-13 18:17:26 +0200

    lstmap in progress

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 13:28:32 by vde-prad          #+#    #+#             */
-/*   Updated: 2022/06/08 13:29:16 by vde-prad         ###   ########.fr       */
+/*   Updated: 2022/06/13 16:49:16 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -191,4 +191,12 @@ void	ft_lstclear(t_list **lst, void (*del)(void *));
 
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 //itera la lista y aplica 'f' a cada nodo
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+//Itera la lista ’lst’ y aplica la función ’f’ al
+//contenido de cada nodo. Crea una lista resultante
+//de la aplicación correcta y sucesiva de la función
+//’f’ sobre cada nodo. La función ’del’ se utiliza
+//para eliminar el contenido de un nodo, si hace
+//falta.
 #endif
