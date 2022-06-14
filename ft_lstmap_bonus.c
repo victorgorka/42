c22a931609384d8a3943583f6e1a09e08bfc8196
Victor gorka De prada arnedo <vde-prad@student.42malaga.com>
2022-06-14 16:20:04 +0200

    lstmap finished

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,16 +6,27 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 16:46:26 by vde-prad          #+#    #+#             */
-/*   Updated: 2022/06/13 18:16:53 by vde-prad         ###   ########.fr       */
+/*   Updated: 2022/06/14 16:16:50 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*res;
+	t_list	*new;
 
-	while ()
+	res = NULL;
+	while (lst)
 	{
-
+		new = ft_lstnew(f(lst->content));
+		if (!new)
+		{
+			ft_lstclear(&res, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&res, new);
+		lst = lst->next;
 	}
+	return (res);
 }
