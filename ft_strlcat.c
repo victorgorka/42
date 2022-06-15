9026b69c0e1a287534dd480867f5f9db96ff532e
Victor gorka De prada arnedo <vde-prad@student.42malaga.com>
2022-06-15 17:10:13 +0200

    strlcat fixed

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,45 +6,51 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 15:28:17 by vde-prad          #+#    #+#             */
-/*   Updated: 2022/05/25 13:45:08 by vde-prad         ###   ########.fr       */
+/*   Updated: 2022/06/15 16:49:07 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t	min_size;
-	size_t	max_size;
-	size_t	final_size;
+	int	n;
 
-	if (dstsize < ft_strlen(dst))
-		return (dstsize + ft_strlen(src));
-	final_size = ft_strlen(dst) + ft_strlen(src);
-	min_size = ft_strlen(dst);
-	max_size = dstsize - ft_strlen(dst) - 1;
-	if (dstsize == 0 || ft_strlen(dst) > dstsize)
-		return (final_size);
-	ft_memcpy(dst + min_size, src, max_size);
-	dst[dstsize - 1] = 0;
-	return (final_size);
+	n = 0;
+	while (*dst != '\0' && dstsize != 0)
+	{
+		dst++;
+		n++;
+		dstsize--;
+	}
+	while (*src != '\0' && dstsize > 1)
+	{
+		*dst++ = *src++;
+		dstsize--;
+		n++;
+	}
+	while (*src)
+	{
+		n++;
+		src++;
+	}
+	if (dstsize)
+		*dst = '\0';
+	return (n);
 }
 /*
 #include<unistd.h>
 #include<stdio.h>
 #include<string.h>
-int	main(void)
+int	main()
 {
-	char	dst1[10] = "a";
-	//char	src1[] = "the cake is a lie !\0I'm hidden lol\r\n";
-	char	dst2[10] = "a";
-	//char	src2[] = "the cake is a lie !\0I'm hidden lol\r\n";
-	//size_t max = strlen("the cake is a lie !\0I'm hidden lol\r\n") + 4;
-	printf("%lu\n",strlcat(dst1, "lorem ipsum dolor sit amet", 6));
-	printf("%zu\n",ft_strlcat(dst2, "lorem ipsum dolor sit amet", 6));
-	puts(dst1);
-	printf("%s\n", dst2);
-	write(1, dst1, 15);
+	char	*dest;
+
+	if (!(dest = (char *)malloc(sizeof(*dest) * 15)) || argc == 1)
+		return (0);
+	memset(dest, 0, 15);
+	memset(dest, 'r', 6);
+	printf("%lu\n", strlcat(dest, "lorem ipsum dolor sit amet", 6));
 	write(1, "\n", 1);
-	write(1, dst2, 30);
+	write(1, dest, 15);
 }
 */
